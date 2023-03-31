39ff30b05e17a825e147acfb4eb14478b087bcc9
Steeven Morales Medina <steeven.morales@confival.com>
2023-03-31 16:27:24 -0500

    inicial

--- obj/Release/net6.0/PubTmp/Out/wwwroot/assets/landing/lib/images-comparsion/twentytwenty.js
+++
@@ -1,4 +0,0 @@
-$(function(){
-    $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({default_offset_pct: 0.6});
-    $(".twentytwenty-container[data-orientation='vertical']").twentytwenty({default_offset_pct: 0.5, orientation: 'vertical'});
-  });
