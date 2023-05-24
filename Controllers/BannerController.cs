bd540e1ba7389ddf51e1b527da7e4cc29bb9f1fc
Steeven Morales Medina <steeven.morales@confival.com>
2023-05-24 12:07:46 -0500

    Banner POST ok

--- Controllers/BannerController.cs
+++ Controllers/BannerController.cs
@@ -1,7 +1,6 @@
-﻿using confinancia.Models.JsonDTO;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
 
 namespace confinancia.Controllers
 {
@@ -27,21 +26,34 @@ namespace confinancia.Controllers
 		[HttpGet]
 		public IActionResult Create()
 		{
-			var token = Request.Cookies[_configuration.GetSection("Variables:Cookie").Value];
-			ViewBag.Token = token;
 			return View();
 		}
 
 		[Authorize]
 		[HttpPost]
-		public int Save(IFormFile obj, string name, string URL)
+		public async Task<bool> Save(IFormFile obj, string name, string URL)
 		{
-			//var file = HttpContext.Request.Form.Files.First();
-			var x = 6;
-			var y = 7;
-			return x + y;
+			var token = Request.Cookies[_configuration.GetSection("Variables:Cookie").Value];
+			var client = new HttpClient();
+			MultipartFormDataContent form = new MultipartFormDataContent();
+			form.Add(new StringContent("1"), "codArchivo");
+			Stream streamPDF = obj.OpenReadStream();
+			if (streamPDF != null)
+			{
+				var contentPDF = new StreamContent(streamPDF);
+				contentPDF.Headers.ContentType = MediaTypeHeaderValue.Parse(obj.ContentType);
+				form.Add(contentPDF, "UrlSoporte", obj.Name);
+			}
+			client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+			var response = await client.PostAsync("https://api2valuezbpm.azurewebsites.net/api/repositorioArchivo?Agrupacion1=" + _configuration.GetSection("LandingPage:BannerInicio:A1").Value + "&Agrupacion2=" + _configuration.GetSection("LandingPage:BannerInicio:A2").Value + "&Agrupacion3=" + _configuration.GetSection("LandingPage:BannerInicio:A3").Value + "&Agrupacion4=" + _configuration.GetSection("LandingPage:BannerInicio:A4").Value + "&Agrupacion5=" + _configuration.GetSection("LandingPage:BannerInicio:A5").Value, form);
+			if (response.IsSuccessStatusCode)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
-
-
 	}
 }
--- wwwroot/assets/js/WebConfival/Banners/Banner.js
+++ wwwroot/assets/js/WebConfival/Banners/Banner.js
@@ -1,21 +1,52 @@
 ﻿function SaveBanner() {
+    var valid = ValidBanner();
+        if (valid == 0) {
+        var nombre = $("#banner-nombre").val();
+        var url = $("#url-banner").val();
+        var file = $("#file-banner").get(0).files[0];
+        var form = new FormData();
+        form.append("obj", file);
+        form.append("name", nombre);
+        form.append("URL", url);
+        $.ajax({
+            type: "POST",
+            url: '/Banner/Save',
+            data: form,
+            cache: false,
+            contentType: false,//stop jquery auto convert form type to default x-www-form-urlencoded
+            processData: false,
+            success: function (result) {
+                if (result == true) {
+                    $("#banner-success").css("display", "block");
+                    $("#form-banner-principal").css("display", "none");
+                }
+            }
+        });
+    }
+}
+
+function ValidBanner() {
+    var count = 0;
     var nombre = $("#banner-nombre").val();
     var url = $("#url-banner").val();
     var file = $("#file-banner").get(0).files[0];
-    var form = new FormData();
-    form.append("obj", file);
-    form.append("name", nombre);
-    form.append("URL", url);
-    $.ajax({
-        type: "POST",
-        url: '/Banner/Save',
-        data: form,
-        cache: false,
-        contentType: false,//stop jquery auto convert form type to default x-www-form-urlencoded
-        processData: false,
-        success: function () {
-            console.log("ok")
-        }
-    });
-
+    if (nombre.length == 0) {
+        $("#invalid-banner-nombre").css("display", "block");
+        count = count + 1;
+    } else {
+        $("#invalid-banner-nombre").css("display", "none");
+    }
+    if (url.length == 0) {
+        $("#invalid-url-banner").css("display", "block");
+        count = count + 1;
+    } else {
+        $("#invalid-url-banner").css("display", "none");
+    }
+    if (file == null) {
+        $("#invalid-file-banner").css("display", "block");
+        count = count + 1;
+    } else {
+        $("#invalid-file-banner").css("display", "none");
+    }
+    return count;
 }
